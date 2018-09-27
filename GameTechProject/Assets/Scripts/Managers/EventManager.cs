0c8314ca5699939117e7668570e2de9b7bf60446
Walter Hill <walterhilldev@gmail.com>
2018-09-27 12:47:32 -0400

    Learned about the event system in Unity, Occlusion Culling, and Animations.

---
+++ GameTechProject/Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Managers
+{
+	public class EventManager : Singleton<EventManager>
+	{
+		public delegate void SampleAction();
+		public static event SampleAction OnSampleAction;
+
+		private const float BUTTON_SIZE = 100.0f;
+
+		private void OnGUI()
+		{
+			if (GUI.Button(new Rect((Screen.width - BUTTON_SIZE) / 2.0f, (Screen.height - BUTTON_SIZE) / 2.0f, BUTTON_SIZE, BUTTON_SIZE), "Hello, World!"))
+			{
+				//If there are no listeners, then the delegate is null so we need to make sure it isn't null before sending.
+				if (OnSampleAction != null)
+				{
+					OnSampleAction();
+				}
+			}
+		}
+	}
+}
---
+++ GameTechProject/Assets/Scripts/Player/MakeRed.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class MakeRed : MonoBehaviour 
+{
+	private MeshRenderer objRenderer;
+
+	private void Start()
+	{
+		objRenderer = GetComponent<MeshRenderer>();
+	}
+
+	private void OnEnable()
+	{
+		Managers.EventManager.OnSampleAction += ChangeColor;
+	}
+
+	private void OnDisable()
+	{
+		Managers.EventManager.OnSampleAction -= ChangeColor;
+	}
+
+	private void ChangeColor()
+	{	
+		objRenderer.material.color = Color.red;
+	}
+}
