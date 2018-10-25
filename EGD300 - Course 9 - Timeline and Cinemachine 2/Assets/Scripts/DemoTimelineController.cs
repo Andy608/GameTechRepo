f086b2cd0ae222d6f045993bba7817d413ab862c
Andy608 <AndrewR608@gmail.com>
2018-10-25 12:40:17 -0400

    Class 8.

---
+++ EGD300 - Course 9 - Timeline and Cinemachine 2/Assets/Scripts/DemoTimelineController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class DemoTimelineController : MonoBehaviour {
+
+	public PlayableDirector aDirector;
+	public TimelineAsset aTimeline;
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			LaunchPlay();
+	}
+
+	void LaunchPlay()
+	{
+		aDirector.Play(aTimeline);
+	}
+}
